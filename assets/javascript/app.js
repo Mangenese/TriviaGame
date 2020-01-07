ae420adaeba198bf86a4356d8ef2bfeb55cf7456
Mangenese <blakemangen20@gmail.com>
2020-01-06 19:36:16 -0700

    added the pseudocode for the app.js file

---
+++ assets/javascript/app.js
@@ -0,0 +1,9 @@
+// create a trivia game 
+// upon webpage load have a clickable button
+// once button is pressed load the game 
+// once game starts start the countdown timer.
+// user has to answer all the questions before time runs out
+// if time runs out, game ends 
+// once game ends calculate total score 
+// total up right to wrong 
+
