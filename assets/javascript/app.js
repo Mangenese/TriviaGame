52f5d9147ff20f61ef8f1dd964f92732cb785b92
Mangenese <blakemangen20@gmail.com>
2020-01-08 08:15:34 -0700

    changed my questions to variables in js and made my html simple with divs that js will modify the DOM with

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -14,9 +14,77 @@ var total = 7;
 var point = 1;
 var highest = total * point;
 
+// set up varibles and objects for our questions
 
-// creating a function to initialize the game
-
-function init() {
-    // set the correct guesses to 0 at start
-}
+var questions = [
+    {
+        question: "Who of the following was not playable in Smash 64?",
+        answers: {
+            a: 'Yoshi',
+            b: 'Pikachu',
+            c: 'Ness',
+            d: 'Peach',
+        },
+        correctAnswer: 'd',
+    },
+    {
+        question: "Who of the following was not playable in Melee?",
+        answers: {
+            a: 'Eliwood',
+            b: 'Mr. Game & Watch',
+            c: 'Bowswer',
+            d: 'Shiek',
+        },
+        correctAnswer: 'a'
+    },
+    {
+        question: "Who of the following was not playable in Brawl?",
+        answers: {
+            a:'Wolf',
+            b:'Lucas',
+            c:'Sonic The Hedgehog',
+            d:'Roy',
+        },
+        correctAnswer: 'd'
+    },
+    {
+        question: "Who of the following was not playable in Smash 4?",
+        answers: {
+            a:'Mega Man',
+            b:'Little Mac',
+            c:'Lucina',
+            d:'Ivysaur',
+        },
+        correctAnswer: 'd'
+    },
+    {
+        question: "Who of the following was not playable in Smash Ultimate?",
+        answers: {
+            a:'Ken',
+            b:'Shadow The Hedgehog',
+            c:'Joker',
+            d:'Simon',
+        },
+        correctAnswer: 'b'
+    },
+    {
+        question: "Which items can you use in Smash Ultimate?",
+        answers: {
+            a:'Pokeball',
+            b:'Party Stick',
+            c:'Dragoon',
+            d:'Beam Sword',
+        },
+        correctAnswer: 'b'
+    },
+    {
+        question: "Which of the following are not assist trophies?",
+        answer: {
+            a:'Black Knight',
+            b:'Waluigi',
+            c:'Dark Samus',
+            d:'Krystal',
+        },
+        correctAnswer: 'c'
+    },
+];
