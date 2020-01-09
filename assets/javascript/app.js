52f1643d84f9a7af729caa1bbe83cb317f2205d6
Mangenese <blakemangen20@gmail.com>
2020-01-08 21:14:59 -0700

    added JS to make it where my questions and answers post to the screen as well as pseudocoded my html for reference

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -16,75 +16,143 @@ var highest = total * point;
 
 // set up varibles and objects for our questions
 
+var number= 10;
+var intervalId;
+
 var questions = [
     {
         question: "Who of the following was not playable in Smash 64?",
-        answers: {
-            a: 'Yoshi',
-            b: 'Pikachu',
-            c: 'Ness',
-            d: 'Peach',
-        },
+        answers: [
+             'Yoshi',
+             'Pikachu',
+             'Ness',
+             'Peach',
+        ],
         correctAnswer: 'd',
     },
     {
         question: "Who of the following was not playable in Melee?",
-        answers: {
-            a: 'Eliwood',
-            b: 'Mr. Game & Watch',
-            c: 'Bowswer',
-            d: 'Shiek',
-        },
+        answers: [
+             'Eliwood',
+             'Mr. Game & Watch',
+             'Bowswer',
+             'Shiek',
+        ],
         correctAnswer: 'a'
     },
     {
         question: "Who of the following was not playable in Brawl?",
-        answers: {
-            a:'Wolf',
-            b:'Lucas',
-            c:'Sonic The Hedgehog',
-            d:'Roy',
-        },
+        answers: [
+            'Wolf',
+            'Lucas',
+            'Sonic The Hedgehog',
+            'Roy',
+],
         correctAnswer: 'd'
     },
     {
         question: "Who of the following was not playable in Smash 4?",
-        answers: {
-            a:'Mega Man',
-            b:'Little Mac',
-            c:'Lucina',
-            d:'Ivysaur',
-        },
+        answers: [
+            'Mega Man',
+            'Little Mac',
+            'Lucina',
+            'Ivysaur',
+        ],
         correctAnswer: 'd'
     },
     {
         question: "Who of the following was not playable in Smash Ultimate?",
-        answers: {
-            a:'Ken',
-            b:'Shadow The Hedgehog',
-            c:'Joker',
-            d:'Simon',
-        },
+        answers: [
+            'Ken',
+            'Shadow The Hedgehog',
+            'Joker',
+            'Simon',
+        ],
         correctAnswer: 'b'
     },
     {
         question: "Which items can you use in Smash Ultimate?",
-        answers: {
-            a:'Pokeball',
-            b:'Party Stick',
-            c:'Dragoon',
-            d:'Beam Sword',
-        },
+        answers: [
+            'Pokeball',
+            'Party Stick',
+            'Dragoon',
+            'Beam Sword',
+        ],
         correctAnswer: 'b'
     },
     {
         question: "Which of the following are not assist trophies?",
-        answer: {
-            a:'Black Knight',
-            b:'Waluigi',
-            c:'Dark Samus',
-            d:'Krystal',
-        },
+        answers: [
+            'Black Knight',
+            'Waluigi',
+            'Dark Samus',
+            'Krystal',
+        ],
         correctAnswer: 'c'
     },
 ];
+
+//function for pasting the questions in html 
+
+
+function run(){
+    clearInterval(intervalId);
+    intervalId =setInterval(decrement ,1000);
+ }
+ 
+ function decrement() {
+     number--;
+     $("#display").html("<h2>" + number + "</h2>");
+     if (number === 0) {
+         // run results
+         stop();
+ 
+         alert("times up");
+     };
+ };
+ 
+ 
+ function stop(){
+     clearInterval(intervalId);
+ } 
+
+function showQuestions(Start) {
+   
+    
+    for(var i = 0; i<questions.length; i++){
+        var h3 = $("<h3>");
+        h3.text(questions[i].question);
+        $("#quiz").append(h3);
+        var ul = $("<ul>");
+
+        for(var j = 0; j<questions[i].answers.length; j++){
+            var li = $("<li <input='radio'>");
+            
+           
+            li.text(questions[i].answers[j]);
+            ul.append(li);
+            $("#quiz").append(ul);
+        }
+    };
+     
+    
+};
+
+
+
+
+run();
+showQuestions();
+
+    
+console.log(questions);
+
+//<form class="questionForm" id="q1" data-question="1">
+{/* <h3>Who of the following was not playable in Smash 64?</h3>  
+<ul>
+    <li><input type="radio" name="q1" value="a">Yoshi</li>
+    <li><input type="radio" name="q1" value="b">Pikachu</li>
+    <li><input type="radio" name="q1" value="c">Ness</li>
+    <li><input type="radio" name="q1" value="d">Peach</li>
+</ul>
+</form> */}
